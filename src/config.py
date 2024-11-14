d705ae0754b153f0e0556851468a385223354211
Mario Agustín Sgró <mario.sgro@ig.edu.ar>
2024-11-14 11:22:25 -0300

    Refactor configuration parameters and update simulation dates in example.cfg

--- src/config.py
+++ src/config.py
@@ -40,12 +40,13 @@ class Configuration:
         vBS_W0=self.getArray('breeding_site','initial_water')
         vBS_mf=self.getArray('breeding_site','manually_filled')
         vBS_b=self.getArray('breeding_site','bare')
+        vBS_r=self.getArray('breeding_site','radius')
         vBS_ef=self.getArray('breeding_site','evaporation_factor')
         initial_condition=self.getArray('simulation','initial_condition')
         alpha0=self.getArray('biology','alpha0')
 
         assert np.all(vBS_d>0),'vBS_d cannot have a zero in it'#not allowed anymore
-        assert len(vBS_d) == len(vBS_h) == len(vBS_s) == len(vBS_W0) == len(vBS_mf) == len(vBS_b) == len(vBS_ef) == len(alpha0),'vBS_d, vBS_h, vBS_s, vBS_W0, vBS_mf and alpha0 must have the same dimension!'
+        assert len(vBS_r) == len(vBS_d) == len(vBS_h) == len(vBS_s) == len(vBS_W0) == len(vBS_mf) == len(vBS_b) == len(vBS_ef) == len(alpha0),'vBS_d, vBS_h, vBS_s, vBS_W0, vBS_mf and alpha0 must have the same dimension!'
         assert abs(1.-np.sum(vBS_d))<1e-10,'sum(vBS_d)=%s!=1'%(np.sum(vBS_d))
         n=len(vBS_d)
         assert len(initial_condition)==5 or len(initial_condition)==6#(E+L+P)+A1+A2 or #(E+L+P)+A1+A2+F
